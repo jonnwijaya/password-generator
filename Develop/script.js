a16d62d64038afbe2093e4c338d30b4f3395014f
Jonathan <jonathanwijayan@gmail.com>
2022-08-19 20:26:27 +1000

    Modified the if statements, added a while statement, and finished the code. -Final Version

--- Develop/script.js
+++ Develop/script.js
@@ -2,62 +2,62 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  
-  var userChoice = window.prompt("Length of password (min. 8 characters and max. 128 characters)");
 
-  var lowcase = confirm("include lowercase character");
+  while (true) {
+    var userChoice = window.prompt("Length of password (min. 8 characters and max. 128 characters)");
 
-  var upcase = confirm("iclude uppercase character");
-
-  var num = confirm("include numeric character");
-
-  var specialcharacter = confirm("include special character");
-
-  if (
-    (lowcase === true) &&
-    (upcase === true) &&
-    (num === true) &&
-    (specialcharacter === true)
-    ) {
-    var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]/^_`{|}~";
-  }
-
-  else if (
-    (lowcase === true) &&
-    (upcase === true) &&
-    (num === true) &&
-    (specialcharacter === false)
-    ) {
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (!userChoice) {
+      userChoice = alert("Please add a value");
     }
 
-  else if (
-    (lowcase === true) &&
-    (upcase === true) &&
-    (num === false) &&
-    (specialcharacter === false)
-    ) {
-      characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (userChoice < 8 || userChoice > 128) {
+      userChoice = alert("Please add a value higher than 8 and lower than 128");
+    }
+
+    if (userChoice >= 8 && userChoice <= 128) {
+      break;
+    }
   }
 
-  else if (
-    (lowcase === true) &&
-    (upcase === false) &&
-    (num === false) &&
-    (specialcharacter === true)
-  ) {
-    characters = "abcdefghijklmnopqrstuvwxyz";
+  var lowCase = confirm("Include lowercase character");
+
+  var upCase = confirm("Include uppercase character");
+
+  var num = confirm("Include numeric character");
+
+  var specialCharacterPrompt = confirm("Include special character");
+
+  var characterArray = [];
+
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+  var numeric = ["1","2","3","4","5","6","7","8","9","0"];
+
+  var specialCharacter = ["!","'",'"',"#","$","%","&","'","(",")","*","+","-",".","/",";",":","<","=",">","?","@"];
+
+  if(lowCase === true) {
+    characterArray = characterArray.concat(lowerCase);
   }
 
-  else {
-    window.alert("error");
+  if(upCase === true) {
+    characterArray = characterArray.concat(upperCase);
+  }
+
+  if(num === true) {
+    characterArray = characterArray.concat(numeric);
+  }
+
+  if(specialCharacterPrompt === true) {
+    characterArray = characterArray.concat(specialCharacter);
   }
 
   var length = userChoice;
-      charset = characters;
+  var charac = characterArray.join("");
       retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+  for (var i = 0, n = charac.length; i < length; ++i) {
+      retVal += charac.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
 }
@@ -68,7 +68,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
